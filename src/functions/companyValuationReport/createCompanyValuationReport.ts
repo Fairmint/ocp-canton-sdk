20460fd81200ad858a2fc997fb5733bbd7a8eae9
HardlyDifficult <hardlydiff@gmail.com>
2025-08-20 10:02:45 -0400

    Disclose the featured app

--- src/functions/companyValuationReport/createCompanyValuationReport.ts
+++ src/functions/companyValuationReport/createCompanyValuationReport.ts
@@ -2,12 +2,30 @@ import { Fairmint } from '@fairmint/open-captable-protocol-daml-js';
 import { LedgerJsonApiClient } from '@fairmint/canton-node-sdk';
 import factoryContractIdData from '@fairmint/open-captable-protocol-daml-js/ocp-factory-contract-id.json';
 import { SubmitAndWaitForTransactionTreeResponse } from '@fairmint/canton-node-sdk/build/src/clients/ledger-json-api/operations';
+import * as damlTypes from '@daml/types';
+
+/**
+ * Details about the FeaturedAppRight contract that need to be disclosed
+ * when exercising the CreateCompanyValuationReport choice. This is required for cross-domain
+ * contract interactions in Canton.
+ */
+export interface FeaturedAppRightContractDetails {
+  /** The contract ID of the FeaturedAppRight contract */
+  contractId: string;
+  /** The serialized created event blob of the contract */
+  createdEventBlob: string;
+  /** The synchronizer ID associated with the contract */
+  synchronizerId: string;
+  /** The template ID of the contract */
+  templateId: string;
+}
 
 export interface CreateCompanyValuationReportParams {
   companyId: string;
   companyValuation: string | number;
   observers?: string[];
-  featuredAppRight: string; // Contract ID of the FeaturedAppRight contract
+  /** Details of the FeaturedAppRight contract for disclosed contracts */
+  featuredAppRightContractDetails: FeaturedAppRightContractDetails;
 }
 
 export interface CreateCompanyValuationReportResult {
@@ -18,6 +36,26 @@ export interface CreateCompanyValuationReportResult {
 /**
  * Create a CompanyValuationReport by exercising the CreateCompanyValuationReport choice
  * on the OCP Factory contract.
+ * 
+ * This function requires the FeaturedAppRight contract details to be provided for disclosed contracts,
+ * which is necessary for cross-domain contract interactions in Canton.
+ * 
+ * @example
+ * ```typescript
+ * const featuredAppRightContractDetails = {
+ *   contractId: "1234567890abcdef",
+ *   createdEventBlob: "serialized_contract_blob_here",
+ *   synchronizerId: "sync_id_here",
+ *   templateId: "FeaturedAppRight:template:id:here"
+ * };
+ * 
+ * const result = await createCompanyValuationReport(client, {
+ *   companyId: "company123",
+ *   companyValuation: "1000000",
+ *   observers: ["observer1", "observer2"],
+ *   featuredAppRightContractDetails
+ * });
+ * ```
  */
 export async function createCompanyValuationReport(
   client: LedgerJsonApiClient,
@@ -35,7 +73,7 @@ export async function createCompanyValuationReport(
       ? params.companyValuation.toString()
       : params.companyValuation,
     observers: params.observers ?? [],
-    featured_app_right: params.featuredAppRight as any
+    featured_app_right: params.featuredAppRightContractDetails.contractId as damlTypes.ContractId<any>
   };
 
   const response = await client.submitAndWaitForTransactionTree({
@@ -47,6 +85,14 @@ export async function createCompanyValuationReport(
           choice: 'CreateCompanyValuationReport',
           choiceArgument: choiceArguments
         }
+      }
+    ],
+    disclosedContracts: [
+      {
+        templateId: params.featuredAppRightContractDetails.templateId,
+        contractId: params.featuredAppRightContractDetails.contractId,
+        createdEventBlob: params.featuredAppRightContractDetails.createdEventBlob,
+        synchronizerId: params.featuredAppRightContractDetails.synchronizerId
       }
     ]
   }) as SubmitAndWaitForTransactionTreeResponse;
